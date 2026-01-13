b59973d2ce0fe83c14e89daa9a1ccf4084ec2adf
Matt Port <mattp0@duck.com>
2026-01-13 23:09:42 +0000

    add fidget and lualine

---
+++ dot-config/nvim/lua/plugins/fidget.lua
@@ -0,0 +1,12 @@
+return {
+    'j-hui/fidget.nvim',
+    opts = {
+        -- Options for fidget.nvim
+        -- The plugin works great with defaults, but you can customize here if needed
+        notification = {
+            window = {
+                winblend = 0, -- Background transparency (0-100)
+            },
+        },
+    },
+}
