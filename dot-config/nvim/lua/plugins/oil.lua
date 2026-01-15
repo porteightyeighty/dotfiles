12e48ae29debd982f842ad65b5c07b3eb5b0cc60
Matt Port <mattp0@duck.com>
2026-01-15 15:24:45 +0000

    add helper functions to oil and configure

--- dot-config/nvim/lua/plugins/oil.lua
+++ dot-config/nvim/lua/plugins/oil.lua
@@ -1,11 +1,55 @@
+-- Drill down through directories that only contain a single subdirectory
+local function get_deepest_dir(path)
+	local entries = vim.fn.readdir(path)
+	-- Filter out hidden files if oil is configured to hide them (default behavior)
+	entries = vim.tbl_filter(function(e)
+		return not vim.startswith(e, '.')
+	end, entries)
+
+	if #entries == 1 then
+		local child_path = path .. '/' .. entries[1]
+		if vim.fn.isdirectory(child_path) == 1 then
+			return get_deepest_dir(child_path)
+		end
+	end
+	return path
+end
+
+local function open_with_drill_down()
+	local oil = require('oil')
+	local entry = oil.get_cursor_entry()
+
+	if entry and entry.type == 'directory' then
+		local dir = oil.get_current_dir()
+		if dir then
+			local target = dir .. entry.name
+			local deepest = get_deepest_dir(target)
+			oil.open(deepest)
+			return
+		end
+	end
+	-- Default behavior for files or if something went wrong
+	oil.select()
+end
+
 return {
-  'stevearc/oil.nvim',
-  ---@module 'oil'
-  ---@type oil.SetupOpts
-  opts = {},
-  -- Optional dependencies
-  dependencies = { { "nvim-mini/mini.icons", opts = {} } },
-  -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
-  lazy = false,
+	'stevearc/oil.nvim',
+	---@module 'oil'
+	---@type oil.SetupOpts
+	opts = {
+		watch_for_changes = true,
+		skip_confirm_for_simple_edits = false,
+		win_options = {
+			winbar = "%{v:lua.require('oil').get_current_dir():gsub('^' .. os.getenv('HOME'), '~')}",
+		},
+		keymaps = {
+			['<CR>'] = { callback = open_with_drill_down, desc = 'Open with drill-down' },
+			['<C-l>'] = false,
+		},
+	},
+	-- Optional dependencies
+	dependencies = { { "nvim-mini/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
+	-- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+	lazy = false,
 }
