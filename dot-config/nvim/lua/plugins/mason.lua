6f5daf0a4e77f0b1be5faf7512aa31ae2f5e477c
Matt Port <mattp0@duck.com>
2026-01-15 12:35:53 +0000

    add mason tool installer and associated java tools

--- dot-config/nvim/lua/plugins/mason.lua
+++ dot-config/nvim/lua/plugins/mason.lua
@@ -2,5 +2,22 @@ return {
     {"mason-org/mason.nvim"},
     {"mason-org/mason-lspconfig.nvim"},
     {"neovim/nvim-lspconfig" },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "mason-org/mason.nvim" },
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    -- Java development
+                    "jdtls",
+                    "java-debug-adapter",
+                    "java-test",
+                    "google-java-format",
+                },
+                auto_update = false,
+                run_on_start = true,
+            })
+        end,
+    },
 }
 
