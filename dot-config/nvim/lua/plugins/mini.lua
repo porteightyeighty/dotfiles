7f81ec1bc02aeba36bb6c3c83d92d4b3149f73fb
Matt Port <mattp0@duck.com>
2026-01-15 18:16:33 +0000

    add cmdline mini tool

--- dot-config/nvim/lua/plugins/mini.lua
+++ dot-config/nvim/lua/plugins/mini.lua
@@ -5,6 +5,7 @@ return {
 		require('mini.snippets').setup({})
 		require('mini.icons').setup({})
 		require('mini.ai').setup({})
+		require('mini.cmdline').setup({})
 		local miniclue = require('mini.clue')
 		miniclue.setup({
 			triggers = {
