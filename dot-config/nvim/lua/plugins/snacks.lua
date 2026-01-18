8f203ba9aa609336b0d4a5b6c78e844c5dc86b56
Matt Port <mattp0@duck.com>
2026-01-18 19:51:17 +0000

    disable statuscolumn
    trying to get to the bottom of what shows symbols on the left hand side.
    doesn't appear to be this, but more investigation needed

--- dot-config/nvim/lua/plugins/snacks.lua
+++ dot-config/nvim/lua/plugins/snacks.lua
@@ -1,20 +1,20 @@
 return {
-  "folke/snacks.nvim",
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-    bigfile = { enabled = true },
-    indent = { enabled = true },
-    input = { enabled = true },
-    notifier = { enabled = true },
-    quickfile = { enabled = true },
-    scope = { enabled = true },
-    scroll = { enabled = true },
-    statuscolumn = { enabled = true },
-    words = { enabled = true },
-  },
+	"folke/snacks.nvim",
+	priority = 1000,
+	lazy = false,
+	---@type snacks.Config
+	opts = {
+		-- your configuration comes here
+		-- or leave it empty to use the default settings
+		-- refer to the configuration section below
+		bigfile = { enabled = true },
+		indent = { enabled = true },
+		input = { enabled = true },
+		notifier = { enabled = true },
+		quickfile = { enabled = true },
+		scope = { enabled = true },
+		scroll = { enabled = true },
+		statuscolumn = { enabled = false },
+		words = { enabled = true },
+	},
 }
