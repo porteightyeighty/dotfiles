8229dcbdb540669f54738eeab4fb70a300053bf1
Matt Port <matt@impactbox.coop>
2026-01-27 21:13:14 +0000

    add surround

--- dot-config/nvim/lua/plugins/mini.lua
+++ dot-config/nvim/lua/plugins/mini.lua
@@ -1,38 +1,38 @@
 return {
-	'nvim-mini/mini.nvim',
+	"nvim-mini/mini.nvim",
 	version = false,
 	config = function()
-		require('mini.ai').setup({})
-		require('mini.diff').setup({})
-		local miniclue = require('mini.clue')
+		require("mini.ai").setup({})
+		require("mini.diff").setup({})
+		local miniclue = require("mini.clue")
 		miniclue.setup({
 			triggers = {
 				-- Leader triggers
-				{ mode = { 'n', 'x' }, keys = '<Leader>' },
+				{ mode = { "n", "x" }, keys = "<Leader>" },
 				-- `[` and `]` keys
-				{ mode = 'n',          keys = '[' },
-				{ mode = 'n',          keys = ']' },
+				{ mode = "n", keys = "[" },
+				{ mode = "n", keys = "]" },
 				-- Built-in completion
-				{ mode = 'i',          keys = '<C-x>' },
+				{ mode = "i", keys = "<C-x>" },
 				-- `g` key
-				{ mode = { 'n', 'x' }, keys = 'g' },
+				{ mode = { "n", "x" }, keys = "g" },
 				-- Marks
-				{ mode = { 'n', 'x' }, keys = "'" },
-				{ mode = { 'n', 'x' }, keys = '`' },
+				{ mode = { "n", "x" }, keys = "'" },
+				{ mode = { "n", "x" }, keys = "`" },
 				-- Registers
-				{ mode = { 'n', 'x' }, keys = '"' },
-				{ mode = { 'i', 'c' }, keys = '<C-r>' },
+				{ mode = { "n", "x" }, keys = '"' },
+				{ mode = { "i", "c" }, keys = "<C-r>" },
 				-- Window commands
-				{ mode = 'n',          keys = '<C-w>' },
+				{ mode = "n", keys = "<C-w>" },
 				-- `z` key
-				{ mode = { 'n', 'x' }, keys = 'z' },
+				{ mode = { "n", "x" }, keys = "z" },
 			},
 
 			window = {
 				delay = 200,
 				config = {
-					width = 'auto'
-				}
+					width = "auto",
+				},
 			},
 			clues = {
 				miniclue.gen_clues.square_brackets(),
@@ -44,19 +44,20 @@ return {
 				miniclue.gen_clues.z(),
 
 				-- Leader group descriptions
-				{ mode = 'n', keys = '<Leader>c', desc = '+Code' },
-				{ mode = 'x', keys = '<Leader>c', desc = '+Code' },
-				{ mode = 'n', keys = '<Leader>d', desc = '+Debug' },
-				{ mode = 'n', keys = '<Leader>j', desc = '+Java' },
-				{ mode = 'x', keys = '<Leader>j', desc = '+Java' },
-				{ mode = 'n', keys = '<Leader>r', desc = '+Refactor' },
-				{ mode = 'n', keys = '<Leader>s', desc = '+Search' },
-				{ mode = 'n', keys = '<Leader>w', desc = '+Window' },
+				{ mode = "n", keys = "<Leader>c", desc = "+Code" },
+				{ mode = "x", keys = "<Leader>c", desc = "+Code" },
+				{ mode = "n", keys = "<Leader>d", desc = "+Debug" },
+				{ mode = "n", keys = "<Leader>j", desc = "+Java" },
+				{ mode = "x", keys = "<Leader>j", desc = "+Java" },
+				{ mode = "n", keys = "<Leader>r", desc = "+Refactor" },
+				{ mode = "n", keys = "<Leader>s", desc = "+Search" },
+				{ mode = "n", keys = "<Leader>w", desc = "+Window" },
 			},
 		})
 		-- require('mini.cmdline').setup({})
-		require('mini.icons').setup({})
-		require('mini.pairs').setup({})
-		require('mini.snippets').setup({})
+		require("mini.icons").setup({})
+		require("mini.pairs").setup({})
+		require("mini.snippets").setup({})
+		require("mini.surround").setup({})
 	end,
 }
