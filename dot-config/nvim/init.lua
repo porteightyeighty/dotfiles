0ebe79efcad38f61c831f919b81549b8fa0e4499
Matt Port <mattp0@duck.com>
2026-01-27 21:30:35 +0000

    cmd height

--- dot-config/nvim/init.lua
+++ dot-config/nvim/init.lua
@@ -12,7 +12,7 @@ vim.opt.cursorline = true
 vim.opt.breakindent = true -- when wrapping text, continue at the same indent level
 vim.opt.wrap = true
 vim.opt.linebreak = true
-vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10    -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.sidescrolloff = 8 -- Keep 8 columns left/right of cursor
 vim.g.have_nerd_font = false
 
@@ -35,44 +35,44 @@ vim.opt.termguicolors = true
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "100"
 vim.opt.showmatch = true
-vim.opt.matchtime = 2 -- How long to show matching bracket
-vim.opt.cmdheight = 0 -- Command line height
+vim.opt.matchtime = 2                             -- How long to show matching bracket
+vim.opt.cmdheight = 1                             -- Command line height
 vim.opt.completeopt = "menuone,noinsert,noselect" -- Completion options
-vim.opt.showmode = false -- Don't show mode in command line
-vim.opt.pumheight = 10 -- Popup menu height
-vim.opt.pumblend = 10 -- Popup menu transparency
-vim.opt.winblend = 0 -- Floating window transparency
-vim.opt.conceallevel = 0 -- Don't hide markup
-vim.opt.concealcursor = "" -- Don't hide cursor line markup
-vim.opt.lazyredraw = true -- Don't redraw during macros
-vim.opt.synmaxcol = 300 -- Syntax highlighting limit
-vim.opt.list = true -- Displays whitespace where the cursor is.
+vim.opt.showmode = false                          -- Don't show mode in command line
+vim.opt.pumheight = 10                            -- Popup menu height
+vim.opt.pumblend = 10                             -- Popup menu transparency
+vim.opt.winblend = 0                              -- Floating window transparency
+vim.opt.conceallevel = 0                          -- Don't hide markup
+vim.opt.concealcursor = ""                        -- Don't hide cursor line markup
+vim.opt.lazyredraw = true                         -- Don't redraw during macros
+vim.opt.synmaxcol = 300                           -- Syntax highlighting limit
+vim.opt.list = true                               -- Displays whitespace where the cursor is.
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
 vim.g.tpipeline_autoembed = 0
 
 -- File handling
-vim.opt.backup = false -- Don't create backup files
+vim.opt.backup = false      -- Don't create backup files
 vim.opt.writebackup = false -- Don't create backup before writing
 vim.opt.swapfile = true
 vim.opt.directory = vim.fn.expand("~/.local/state/nvim/swap") .. "//"
-vim.opt.undofile = true -- Persistent undo
+vim.opt.undofile = true                           -- Persistent undo
 vim.opt.undodir = vim.fn.expand("~/.vim/undodir") -- Undo directory
-vim.opt.updatetime = 300 -- Faster completion
-vim.opt.timeoutlen = 500 -- Key timeout duration
-vim.opt.ttimeoutlen = 0 -- Key code timeout
-vim.opt.autoread = true -- Auto reload files changed outside vim
-vim.opt.autowrite = false -- Don't auto save
+vim.opt.updatetime = 300                          -- Faster completion
+vim.opt.timeoutlen = 500                          -- Key timeout duration
+vim.opt.ttimeoutlen = 0                           -- Key code timeout
+vim.opt.autoread = true                           -- Auto reload files changed outside vim
+vim.opt.autowrite = false                         -- Don't auto save
 
 -- Behavior settings
-vim.opt.hidden = true -- Allow hidden buffers
-vim.opt.errorbells = false -- No error bells
-vim.opt.backspace = "indent,eol,start" -- Better backspace behavior
-vim.opt.autochdir = false -- Don't auto change directory
-vim.opt.selection = "exclusive" -- Selection behavior
-vim.opt.mouse = "a" -- Enable mouse support
+vim.opt.hidden = true                   -- Allow hidden buffers
+vim.opt.errorbells = false              -- No error bells
+vim.opt.backspace = "indent,eol,start"  -- Better backspace behavior
+vim.opt.autochdir = false               -- Don't auto change directory
+vim.opt.selection = "exclusive"         -- Selection behavior
+vim.opt.mouse = "a"                     -- Enable mouse support
 vim.opt.clipboard:append("unnamedplus") -- Use system clipboard
-vim.opt.modifiable = true -- Allow buffer modifications
-vim.opt.encoding = "UTF-8" -- Set encoding
+vim.opt.modifiable = true               -- Allow buffer modifications
+vim.opt.encoding = "UTF-8"              -- Set encoding
 
 -- Split behaviour
 vim.opt.splitright = true -- vertical splits
