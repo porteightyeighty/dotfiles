237b5cc2be987b9cd90a25ff9ccaf2aae0037880
Matt Port <matt@impactbox.coop>
2026-01-27 21:13:23 +0000

    Wezterm

---
+++ dot-wezterm.lua
@@ -0,0 +1,10 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+-- config here
+--
+config.color_scheme = "Everforest Dark (Gogh)"
+config.enable_tab_bar = false
+--
+return config
