46d26c12f0eed54abedd982587bc189a1a902de0
Matt Port <matt@impactbox.coop>
2026-03-10 21:53:40 +0000

    change lualine colorscheme to dracula and add tpipeline back in

--- dot-config/nvim/lua/plugins/lualine.lua
+++ dot-config/nvim/lua/plugins/lualine.lua
@@ -1,5 +1,9 @@
 return {
-	'nvim-lualine/lualine.nvim',
-	dependencies = { 'nvim-tree/nvim-web-devicons' },
-	opts = {}
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	opts = {
+		options = {
+			theme = "dracula",
+		},
+	},
 }
