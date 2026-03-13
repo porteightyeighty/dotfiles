db0fa3f23e38100b56b10592dfaf43c789751cfd
Matt Port <matt@impactbox.coop>
2026-03-13 13:52:11 +0000

    update wezterm colorscheme

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 -- config here
 --
-config.color_scheme = "Everforest Dark (Gogh)"
+config.color_scheme = "Blue Dolphin (Gogh)"
 config.enable_tab_bar = false
 --
 return config
