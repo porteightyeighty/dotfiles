1a27d2c1223b78ef8247bfa8760886fa4120c275
Matt Port <matt@impactbox.coop>
2026-03-13 13:40:44 +0000

    add lualine support for sf

--- dot-config/nvim/lua/plugins/lualine.lua
+++ dot-config/nvim/lua/plugins/lualine.lua
@@ -5,5 +5,27 @@ return {
 		options = {
 			theme = "dracula",
 		},
+		sections = {
+			lualine_x = {
+				{
+					function()
+						local sf_ok, sf = pcall(require, "sf")
+						if sf_ok and sf.get_target_org then
+							local org = sf.get_target_org()
+							if org and org ~= "" then
+								return "SF: " .. org
+							end
+						end
+						return ""
+					end,
+					cond = function()
+						return vim.fn.findfile("sfdx-project.json", ".;") ~= ""
+					end,
+				},
+				"encoding",
+				"fileformat",
+				"filetype",
+			},
+		},
 	},
 }
