0c260443b72c9c30d88957064a441979cc78e417
Matt Port <matt@impactbox.coop>
2026-03-16 16:52:05 +0000

    update wezterm colorscheme

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -4,7 +4,7 @@ local config = wezterm.config_builder()
 
 -- config here
 --
-config.color_scheme = "Blue Dolphin (Gogh)"
+config.color_scheme = "iTerm2 Solarized Light"
 config.enable_tab_bar = false
 --
 return config
