8d89a1e4c273f90f97c64a896d34dd7e16260043
Matt Port <matt@impactbox.coop>
2026-03-17 09:51:29 +0000

    add Salesforce New group to mini.clue

--- dot-config/nvim/lua/plugins/mini.lua
+++ dot-config/nvim/lua/plugins/mini.lua
@@ -51,7 +51,7 @@ return {
 				{ mode = "n", keys = "<Leader>s", desc = "+Search" },
 				{ mode = "n", keys = "<Leader>w", desc = "+Window" },
 				{ mode = "n", keys = "<Leader>S", desc = "+Salesforce" },
-			{ mode = "n", keys = "<Leader>Sn", desc = "+New" },
+				{ mode = "n", keys = "<Leader>Sn", desc = "+New" },
 			},
 		})
 		-- require('mini.cmdline').setup({})
