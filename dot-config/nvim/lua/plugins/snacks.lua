d5124f4503145dff656ac6f92795b93f7ad8b2f4
Matt Port <mattp0@duck.com>
2026-04-26 11:15:08 +0100

    replace oil with snacks explorer

--- dot-config/nvim/lua/plugins/snacks.lua
+++ dot-config/nvim/lua/plugins/snacks.lua
@@ -3,19 +3,20 @@ return {
 	priority = 1000,
 	lazy = false,
 	keys = {
-		{ "<leader>sh", function() Snacks.picker.help() end, desc = "[S]earch [H]elp" },
-		{ "<leader>sk", function() Snacks.picker.keymaps() end, desc = "[S]earch [K]eymaps" },
-		{ "<leader>sf", function() Snacks.picker.files() end, desc = "[S]earch [F]iles" },
-		{ "<leader>ss", function() Snacks.picker.pickers() end, desc = "[S]earch [S]elect Picker" },
-		{ "<leader>sw", function() Snacks.picker.grep_word() end, desc = "[S]earch current [W]ord" },
-		{ "<leader>sg", function() Snacks.picker.grep() end, desc = "[S]earch by [G]rep" },
-		{ "<leader>sd", function() Snacks.picker.diagnostics() end, desc = "[S]earch [D]iagnostics" },
-		{ "<leader>sr", function() Snacks.picker.resume() end, desc = "[S]earch [R]esume" },
-		{ "<leader>s.", function() Snacks.picker.recent() end, desc = '[S]earch Recent Files ("." for repeat)' },
-		{ "<leader>sn", function() Snacks.picker.grep({ cwd = vim.fn.stdpath("config") }) end, desc = "[S]earch [N]eovim Files" },
-		{ "<leader><leader>", function() Snacks.picker.buffers() end, desc = "[ ] Find existing buffers" },
-		{ "<leader>/", function() Snacks.picker.lines() end, desc = "[/] Fuzzy Find in Buffer" },
-		{ "<leader>nh", function() Snacks.notifier.show_history() end, desc = "[N]otification [H]istory" },
+		{ "<leader>sh",       function() Snacks.picker.help() end,                                   desc = "[S]earch [H]elp" },
+		{ "<leader>sk",       function() Snacks.picker.keymaps() end,                                desc = "[S]earch [K]eymaps" },
+		{ "<leader>sf",       function() Snacks.picker.files() end,                                  desc = "[S]earch [F]iles" },
+		{ "<leader>ss",       function() Snacks.picker.pickers() end,                                desc = "[S]earch [S]elect Picker" },
+		{ "<leader>sw",       function() Snacks.picker.grep_word() end,                              desc = "[S]earch current [W]ord" },
+		{ "<leader>sg",       function() Snacks.picker.grep() end,                                   desc = "[S]earch by [G]rep" },
+		{ "<leader>sd",       function() Snacks.picker.diagnostics() end,                            desc = "[S]earch [D]iagnostics" },
+		{ "<leader>sr",       function() Snacks.picker.resume() end,                                 desc = "[S]earch [R]esume" },
+		{ "<leader>s.",       function() Snacks.picker.recent() end,                                 desc = '[S]earch Recent Files ("." for repeat)' },
+		{ "<leader>sn",       function() Snacks.picker.grep({ cwd = vim.fn.stdpath("config") }) end, desc = "[S]earch [N]eovim Files" },
+		{ "<leader><leader>", function() Snacks.picker.buffers() end,                                desc = "[ ] Find existing buffers" },
+		{ "<leader>/",        function() Snacks.picker.lines() end,                                  desc = "[/] Fuzzy Find in Buffer" },
+		{ "<leader>nh",       function() Snacks.notifier.show_history() end,                         desc = "[N]otification [H]istory" },
+		{ "\\",               function() Snacks.explorer.open() end }
 	},
 	---@type snacks.Config
 	opts = {
@@ -23,6 +24,7 @@ return {
 		-- or leave it empty to use the default settings
 		-- refer to the configuration section below
 		bigfile = { enabled = true },
+		explorer = { enabled = true },
 		indent = { enabled = true },
 		input = { enabled = true },
 		notifier = { enabled = true },
