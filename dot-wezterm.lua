602366a181fae403eb79114017b0f9cfd16128dc
Matt Port <mattp0@duck.com>
2026-04-27 22:42:44 +0100

    add alt handling

--- dot-wezterm.lua
+++ dot-wezterm.lua
@@ -2,9 +2,9 @@ local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
--- config here
---
 config.color_scheme = "iTerm2 Solarized Light"
 config.enable_tab_bar = false
---
+
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = false
 return config
