82609c636e05ba70b1b161f97a6421d7a0c2df56
Matt Port <mattp0@duck.com>
2026-05-18 21:22:55 +0100

    add xmllint

--- dot-config/nvim/lua/plugins/conform.lua
+++ dot-config/nvim/lua/plugins/conform.lua
@@ -40,6 +40,7 @@ return { -- Autoformat
 			python = { "ruff" },
 			apex = { "prettier" },
 			java = { "google-java-format" },
+			xml = { "xmllint" },
 			--
 			-- You can use 'stop_after_first' to run the first available formatter from the list
 			-- javascript = { "prettierd", "prettier", stop_after_first = true },
