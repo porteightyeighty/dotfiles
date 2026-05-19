b7ccb1d84ded1ca58d405dc1dd029386ee86de51
matt port <mattp0@duck.com>
2026-05-19 14:24:58 +0100

    add mermaid and markdown_inline to treesitter

--- dot-config/nvim/lua/plugins/treesitter.lua
+++ dot-config/nvim/lua/plugins/treesitter.lua
@@ -1,42 +1,44 @@
 return {
-  'nvim-treesitter/nvim-treesitter',
-  event = { 'BufReadPost', 'BufNewFile' },
-  build = ':TSUpdate',
-  config = function()
-    require("nvim-treesitter").install({
-      "apex",
-      "bash",
-      "c",
-      "css",
-      "html",
-      "java",
-      "javascript",
-      "json",
-      "lua",
-      "markdown",
-      "python",
-      "rust",
-      "sflog",
-      "soql",
-      "sosl",
-      "sql",
-      "tmux",
-      "toml",
-      "typescript",
-      "vim",
-      "vimdoc",
-      "vue",
-      "xml",
-      "yaml",
-      "zig",
-      "zsh",
-    })
+	'nvim-treesitter/nvim-treesitter',
+	event = { 'BufReadPost', 'BufNewFile' },
+	build = ':TSUpdate',
+	config = function()
+		require("nvim-treesitter").install({
+			"apex",
+			"bash",
+			"c",
+			"css",
+			"html",
+			"java",
+			"javascript",
+			"json",
+			"lua",
+			"markdown",
+			"markdown_inline",
+			"mermaid",
+			"python",
+			"rust",
+			"sflog",
+			"soql",
+			"sosl",
+			"sql",
+			"tmux",
+			"toml",
+			"typescript",
+			"vim",
+			"vimdoc",
+			"vue",
+			"xml",
+			"yaml",
+			"zig",
+			"zsh",
+		})
 
-    -- nvim-treesitter main branch requires manual start per filetype
-    vim.api.nvim_create_autocmd("FileType", {
-      callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
-      end,
-    })
-  end,
+		-- nvim-treesitter main branch requires manual start per filetype
+		vim.api.nvim_create_autocmd("FileType", {
+			callback = function(args)
+				pcall(vim.treesitter.start, args.buf)
+			end,
+		})
+	end,
 }
